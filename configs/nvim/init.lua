06498a65b323f19ccfd4ca75b6c05d1826186df4
GalenYost <brawlstarser086@gmail.com>
2025-11-11 16:02:24 +0200

    nvim: removed some plugins (including telescope)

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -21,18 +21,13 @@ vim.g.maplocalleader = ' '
 -- plugins
 vim.pack.add({
     { src = 'https://github.com/vague-theme/vague.nvim' },
-    { src = 'https://github.com/nvim-tree/nvim-web-devicons' },
 
-    { src = 'https://github.com/nvim-treesitter/nvim-treesitter',         version = 'main' },
+    { src = 'https://github.com/nvim-treesitter/nvim-treesitter',       version = 'main' },
     { src = 'https://github.com/saghen/blink.cmp' },
     { src = 'https://github.com/rachartier/tiny-inline-diagnostic.nvim' },
 
     { src = 'https://github.com/nvim-mini/mini.nvim' },
     { src = 'https://github.com/stevearc/oil.nvim' },
-
-    { src = 'https://github.com/nvim-telescope/telescope.nvim' },
-    { src = 'https://github.com/nvim-telescope/telescope-fzf-native.nvim' },
-    { src = 'https://github.com/nvim-lua/plenary.nvim' },
 })
 
 -- oil
@@ -64,49 +59,7 @@ require 'mini.comment'.setup {
         comment_visual = "<leader>c",
     },
 }
-
--- telescope
-local telescope = require('telescope')
-
-telescope.setup {
-    defaults = {
-        vimgrep_arguments = {
-            'rg', '--color=never', '--no-heading',
-            '--with-filename', '--line-number', '--column', '--smart-case'
-        },
-        file_ignore_patterns = {
-            "node_modules", ".git/", "target/", "dist/", "build/"
-        },
-        preview = {
-            filesize_limit = 1,
-        },
-        mappings = {
-            i = {
-                ["<C-u>"] = false,
-                ["<C-d>"] = false,
-            },
-        },
-    },
-    pickers = {
-        find_files = {
-            find_command = { 'fd', '--type', 'f', '--hidden', '--exclude', '.git' },
-            previewer = true,
-        },
-        buffers = {
-            previewer = false,
-        },
-    },
-    extensions = {
-        fzf = {
-            fuzzy = false,
-            override_generic_sorter = true,
-            override_file_sorter = true,
-            case_mode = "smart_case",
-        },
-    },
-}
-
-telescope.load_extension('fzf')
+require 'mini.pick'.setup {}
 
 -- tree-sitter
 require 'nvim-treesitter'.setup {
@@ -214,13 +167,10 @@ vim.keymap.set("n", "<ESC>", vim.cmd.nohlsearch)
 -- oil open parent dir
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
--- mini grep picker
+-- mini pickers
+vim.keymap.set('n', '<leader>f', require 'mini.pick'.builtin.files, { desc = "Find files" })
 vim.keymap.set('n', '<leader>g', require 'mini.pick'.builtin.grep_live)
 
 -- lsp interactions
 vim.keymap.set("n", "gd", vim.lsp.buf.definition)
 vim.keymap.set("n", "rn", vim.lsp.buf.rename)
-
--- telescope
-vim.keymap.set('n', '<leader>f', require('telescope.builtin').find_files, { desc = "Find files" })
-vim.keymap.set('n', '<leader>g', require('telescope.builtin').live_grep, { desc = "Live grep" })
